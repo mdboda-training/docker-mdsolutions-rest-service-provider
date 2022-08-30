f2dae048073e85f9e2a59ab0a823ff712a6566d0
NAVEEN <boda.gadhilinga@gmail.com>
2022-08-30 20:39:36 +0530

    committing changes on dcoker

--- src/main/java/com/mdsolutions/controller/UserController.java
+++ src/main/java/com/mdsolutions/controller/UserController.java
@@ -2,6 +2,8 @@ package com.mdsolutions.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,32 +18,39 @@ import com.mdsolutions.services.UserService;
 @RestController
 public class UserController {
 
+	private Logger logger = LoggerFactory.getLogger(UserController.class);
+	
 	@Autowired
 	private UserService userService;
 	
 	@Autowired
 	Environment environment;
 
-	@GetMapping("/user/getUsers")
+	@GetMapping("/docker-mdsolutions-rest-service-provider/user/getUsers")
 	public UserDto getUsers(@RequestParam Integer userId) {
+		logger.info("Invoking request /mdsolutions-rest-service-provider/user/getUsers by {} id start",userId);
+		
 		UserDto userDto= userService.getUser(userId);
 		userDto.setMessage("Port: "+environment.getProperty("server.port"));
+		logger.info("Invoking request /mdsolutions-rest-service-provider/user/getUsers by {} id end", userId);
 		return userDto;
 	}
 
-	@GetMapping("/user/getAllUsers")
+	@GetMapping("/docker-mdsolutions-rest-service-provider/user/getAllUsers")
 	public List<UserDto> getUsers() {
+		logger.info("Invoking request /mdsolutions-rest-service-provider/user/getUsers Start");
 		List<UserDto> userDtoList = userService.getUsers();
+		logger.info("Invoking request /mdsolutions-rest-service-provider/user/getUsers End");
 		return userDtoList;
 	}
 
-	@GetMapping("/user/deleteUser")
+	@GetMapping("/docker-mdsolutions-rest-service-provider/user/deleteUser")
 	public String getDeleteUser(@RequestParam Integer userId) {
 		userService.deleteUser(userId);
 		return "User Deleted Successfully";
 	}
 	
-	@PostMapping("/user/createUser")
+	@PostMapping("/docker-mdsolutions-rest-service-provider/user/createUser")
 	public UserDto createUser(@RequestBody UserDto userDto) {
 		return userService.createUser(userDto);
 	}
